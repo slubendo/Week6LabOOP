d8b1238b92ea43852d2326a31768c13248a390d1
slubendo <stephane.lubendo@gmail.com>
2023-02-26 16:45:18 -0800

    FIXED failureMsg

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -13,6 +13,7 @@ router.post(
   passport.authenticate("local", {
     successRedirect: "/dashboard",
     failureRedirect: "/auth/login",
+    failureMessage: "Incorrect credentials, try again"
     /* FIX ME: 😭 failureMsg needed when login fails */
   })
 );
